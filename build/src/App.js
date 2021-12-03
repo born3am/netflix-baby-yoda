73ad02765db3a39466b199680e3a1c569062449b
born3am <psmaranhao@gmail.com>
2021-12-03 16:24:18 +0100

    update

---
+++ build/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+//import Section from "./components/Section/Section";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Movies from "./pages/Movies/Movies";
+import TvShows from "./pages/TvShows/TvShows";
+import NotFound from "./pages/NotFound/NotFound";
+import Movie from "./pages/Movie/Movie";
+import TvShow from "./pages/TvShow/TvShow";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="Container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movie/:pageId" element={<Movie />} />
+          <Route path="/tv-shows" element={<TvShows />} />
+          <Route path="/tv-show/:pageId" element={<TvShow />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ build/src/components/Section/Section.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Slider from "react-slick";
+import Card from "./Card/Card";
+import "./Section.css";
+export default function Section({ title, data, content }) {
+  console.log(title, data);
+
+  return (
+    <section className="Section">
+      <h4>{title}</h4>
+      <Slider slidesToShow={3} slidesToScroll={3}>
+        {data?.map((currentValue) => (
+          <Card content={content} key={currentValue.id} movie={currentValue} />
+        ))}
+      </Slider>
+    </section>
+  );
+}
